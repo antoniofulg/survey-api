205ed145be2871b5f40868770c24f2221eb95d8d
Antonio Fulgêncio de Lima <antoniofulg@gmail.com>
2022-12-12 17:39:45 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -23,7 +23,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
         id: 'valid_id',
         name: account.name,
         email: account.email,
-        password: `hashed_${account.password}`,
+        password: account.password,
       }
       return new Promise((resolve) => resolve(fakeAccount))
     }
@@ -104,5 +104,21 @@ describe('DbAddAccount UseCase', () => {
     }
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_valid_password',
+    })
   })
 })
