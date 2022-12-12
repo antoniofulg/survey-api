2cd8dc36340101acbf2e994fa39b97fedc5cb232
Antonio Fulgêncio de Lima <antoniofulg@gmail.com>
2022-12-12 17:36:23 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -91,4 +91,18 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_valid_password',
     })
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { addAccountRepositoryStub, sut } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
