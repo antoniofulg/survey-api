5e454c8557c65263bef583e337ec70e9970ab0cf
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-01-02 06:49:37 -0300

    fix: update integration coverage

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,3 +1,7 @@
 const config = require('./jest.config')
 config.testMatch = ['**/*.test.ts']
+config.collectCoverageFrom = [
+  ...config.collectCoverageFrom,
+  '!<rootDir>/src/**/*.spec.ts',
+]
 module.exports = config
