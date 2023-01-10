33be5a31b1112dd4d0ce53f4b811e96a044d6b80
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-01-10 10:29:54 -0300

    feat: add LoginFactory

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,21 +1,29 @@
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '@/infra/db/mongodb/log/log-mongo-repository'
-import { SignUpController } from '@/presentation/controllers/signup/signup-controller'
-import { Controller } from '@/presentation/protocols'
+import env from '@/main/config/env'
 import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
+import { LoginController } from '@/presentation/controllers/login/login-controller'
+import { Controller } from '@/presentation/protocols'
 import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLoginController = (): Controller => {
   const SALT = 12
   const bcryptAdapter = new BcryptAdapter(SALT)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
   const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(
-    dbAddAccount,
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+  const loginController = new LoginController(
+    dbAuthentication,
     makeLoginValidation()
   )
   const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return new LogControllerDecorator(loginController, logMongoRepository)
 }
